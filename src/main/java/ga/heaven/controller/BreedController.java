0a184de865c42a44e7bcadcd9f2e4f5cd0a7b389
Telion <www.neon@mail.ru>
2023-02-26 17:02:17 +0500

    Added API documentation for ShelterController and BreedController

--- src/main/java/ga/heaven/controller/BreedController.java
+++ src/main/java/ga/heaven/controller/BreedController.java
@@ -1,8 +1,14 @@
 package ga.heaven.controller;
 
 import ga.heaven.model.Breed;
+import ga.heaven.model.Shelter;
 import ga.heaven.service.BreedService;
-import org.springframework.http.HttpStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,14 +23,46 @@ public class BreedController {
         this.breedService = breedService;
     }
 
+    @Operation(
+            tags = "Breed",
+            summary = "search for all breed in the database.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Found breeds",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = List.class)
+                            )
+                    )
+            }
+    )
     @GetMapping
     public ResponseEntity<List<Breed>> readAll() {
         List<Breed> breeds = breedService.findAll();
         return ResponseEntity.ok(breeds);
     }
 
+    @Operation(
+            tags = "Breed",
+            summary = "search breed by its ID in the database.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Found breed",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Breed.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "No breed found with this id"
+                    )
+            }
+    )
     @GetMapping("/{id}")
-    public ResponseEntity<Breed> readById(@PathVariable Long id) {
+    public ResponseEntity<Breed> readById(@Parameter(description = "id of the breed to find") @PathVariable Long id) {
         Breed breed = breedService.findById(id);
         if (breed == null) {
             return ResponseEntity.notFound().build();
@@ -32,12 +70,58 @@ public class BreedController {
         return ResponseEntity.ok(breed);
     }
 
+    @Operation(
+            tags = "Breed",
+            summary = "add new breed in the database.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Added breed",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Breed.class)
+                            )
+                    )
+            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "the id field is ignored, id is automatically incremented by 1",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Breed.class)
+                    )
+            )
+    )
     @PostMapping
     public ResponseEntity<Breed> create(@RequestBody Breed breed) {
         Breed createdBreed = breedService.create(breed);
         return ResponseEntity.ok(createdBreed);
     }
 
+    @Operation(
+            tags = "Breed",
+            summary = "update breed by its ID in the database.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Updated breed",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Breed.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "No breed found with this id"
+                    )
+            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "if a non-existent id is entered, a 404 error will be returned",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Breed.class)
+                    )
+            )
+    )
     @PutMapping
     public ResponseEntity<Breed> update(@RequestBody Breed breed) {
         Breed updatedBreed = breedService.update(breed);
@@ -47,8 +131,33 @@ public class BreedController {
         return ResponseEntity.ok(updatedBreed);
     }
 
+    @Operation(
+            tags = "Breed",
+            summary = "delete breed by its ID in the database.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Deleted breed",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Breed.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "No breed found with this id"
+                    )
+            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "if a non-existent id is entered, a 404 error will be returned",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Breed.class)
+                    )
+            )
+    )
     @DeleteMapping("/{id}")
-    public ResponseEntity<Breed> delete(@PathVariable Long id) {
+    public ResponseEntity<Breed> delete(@Parameter(description = "id of the breed to delete") @PathVariable Long id) {
         Breed deletedBreed = breedService.delete(id);
         if (deletedBreed == null) {
             return ResponseEntity.notFound().build();
--- src/main/java/ga/heaven/controller/ShelterController.java
+++ src/main/java/ga/heaven/controller/ShelterController.java
@@ -2,7 +2,12 @@ package ga.heaven.controller;
 
 import ga.heaven.model.Shelter;
 import ga.heaven.service.ShelterService;
-import org.springframework.http.HttpStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,14 +22,46 @@ public class ShelterController {
         this.shelterService = shelterService;
     }
 
+    @Operation(
+            tags = "Shelter",
+            summary = "search for all shelters in the database.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Found shelters",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = List.class)
+                            )
+                    )
+            }
+    )
     @GetMapping
     public ResponseEntity<List<Shelter>> findAll() {
         List<Shelter> shelters = shelterService.findAll();
         return ResponseEntity.ok(shelters);
     }
 
+    @Operation(
+            tags = "Shelter",
+            summary = "search shelter by its ID in the database.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Found shelter",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Shelter.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "No shelter found with this id"
+                    )
+            }
+    )
     @GetMapping("/{id}")
-    public ResponseEntity<Shelter> findById(@PathVariable Long id) {
+    public ResponseEntity<Shelter> findById(@Parameter(description = "id of the shelter to find") @PathVariable Long id) {
         Shelter shelter = shelterService.findById(id);
         if (shelter == null) {
             return ResponseEntity.notFound().build();
@@ -32,12 +69,58 @@ public class ShelterController {
         return ResponseEntity.ok(shelter);
     }
 
+    @Operation(
+            tags = "Shelter",
+            summary = "add new shelter in the database.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Added shelter",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Shelter.class)
+                            )
+                    )
+            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "the id field is ignored, id is automatically incremented by 1",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Shelter.class)
+                    )
+            )
+    )
     @PostMapping
     public ResponseEntity<Shelter> create(@RequestBody Shelter shelter) {
         Shelter createdShelter = shelterService.create(shelter);
         return ResponseEntity.ok(createdShelter);
     }
 
+    @Operation(
+            tags = "Shelter",
+            summary = "update shelter by its ID in the database.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Updated shelter",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Shelter.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "No shelter found with this id"
+                    )
+            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "if a non-existent id is entered, a 404 error will be returned",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Shelter.class)
+                    )
+            )
+    )
     @PutMapping
     public ResponseEntity<Shelter> update(@RequestBody Shelter shelter) {
         Shelter updatedShelter = shelterService.update(shelter);
@@ -47,8 +130,33 @@ public class ShelterController {
         return ResponseEntity.ok(updatedShelter);
     }
 
+    @Operation(
+            tags = "Shelter",
+            summary = "delete shelter by its ID in the database.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Deleted shelter",
+                            content = @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = Shelter.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "No shelter found with this id"
+                    )
+            },
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "if a non-existent id is entered, a 404 error will be returned",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = Shelter.class)
+                    )
+            )
+    )
     @DeleteMapping("/{id}")
-    public ResponseEntity<Shelter> delete(@PathVariable Long id) {
+    public ResponseEntity<Shelter> delete(@Parameter(description = "id of the shelter to delete") @PathVariable Long id) {
         Shelter deletedShelter = shelterService.delete(id);
         if (deletedShelter == null) {
             return ResponseEntity.notFound().build();
