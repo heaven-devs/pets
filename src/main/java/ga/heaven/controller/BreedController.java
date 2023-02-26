e144d0c5d7b1425365d7bc0dca681da00ab5ecb4
Telion <www.neon@mail.ru>
2023-02-26 15:49:21 +0500

    Refactoring CRUD operations in ShelterController and BreedController

--- src/main/java/ga/heaven/controller/BreedController.java
+++ src/main/java/ga/heaven/controller/BreedController.java
@@ -2,13 +2,14 @@ package ga.heaven.controller;
 
 import ga.heaven.model.Breed;
 import ga.heaven.service.BreedService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
+import java.util.List;
 
 @RestController
-@RequestMapping("breed")
+@RequestMapping("/breed")
 public class BreedController {
     private final BreedService breedService;
 
@@ -17,14 +18,17 @@ public class BreedController {
     }
 
     @GetMapping
-    public ResponseEntity<Collection<Breed>> findAll() {
-        Collection<Breed> breeds = breedService.findAll();
+    public ResponseEntity<List<Breed>> readAll() {
+        List<Breed> breeds = breedService.findAll();
         return ResponseEntity.ok(breeds);
     }
 
-    @GetMapping("{id}")
-    public ResponseEntity<Breed> findById(@PathVariable Long id) {
+    @GetMapping("/{id}")
+    public ResponseEntity<Breed> readById(@PathVariable Long id) {
         Breed breed = breedService.findById(id);
+        if (breed == null) {
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok(breed);
     }
 
@@ -37,11 +41,18 @@ public class BreedController {
     @PutMapping
     public ResponseEntity<Breed> update(@RequestBody Breed breed) {
         Breed updatedBreed = breedService.update(breed);
+        if (updatedBreed == null) {
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok(updatedBreed);
     }
 
-    @DeleteMapping("{id}")
-    public void delete(@PathVariable Long id) {
-        breedService.delete(id);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Breed> delete(@PathVariable Long id) {
+        Breed deletedBreed = breedService.delete(id);
+        if (deletedBreed == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(deletedBreed);
     }
 }
--- src/main/java/ga/heaven/controller/ShelterController.java
+++ src/main/java/ga/heaven/controller/ShelterController.java
@@ -2,13 +2,14 @@ package ga.heaven.controller;
 
 import ga.heaven.model.Shelter;
 import ga.heaven.service.ShelterService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
+import java.util.List;
 
 @RestController
-@RequestMapping("shelter")
+@RequestMapping("/shelter")
 public class ShelterController {
     private final ShelterService shelterService;
 
@@ -17,14 +18,17 @@ public class ShelterController {
     }
 
     @GetMapping
-    public ResponseEntity<Collection<Shelter>> findAll() {
-        Collection<Shelter> shelters = shelterService.findAll();
+    public ResponseEntity<List<Shelter>> findAll() {
+        List<Shelter> shelters = shelterService.findAll();
         return ResponseEntity.ok(shelters);
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Shelter> findById(@PathVariable Long id) {
         Shelter shelter = shelterService.findById(id);
+        if (shelter == null) {
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok(shelter);
     }
 
@@ -37,12 +41,19 @@ public class ShelterController {
     @PutMapping
     public ResponseEntity<Shelter> update(@RequestBody Shelter shelter) {
         Shelter updatedShelter = shelterService.update(shelter);
+        if (updatedShelter == null) {
+            return ResponseEntity.notFound().build();
+        }
         return ResponseEntity.ok(updatedShelter);
     }
 
-    @DeleteMapping("{id}")
-    public void delete(@PathVariable Long id) {
-        shelterService.delete(id);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Shelter> delete(@PathVariable Long id) {
+        Shelter deletedShelter = shelterService.delete(id);
+        if (deletedShelter == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(deletedShelter);
     }
 
 
