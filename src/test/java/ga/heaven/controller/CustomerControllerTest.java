80b34e04cb1eabe5e7fb741e7c3948ae52e14178
Alex Turaev <altex_76@mail.ru>
2023-03-05 13:55:09 +0200

    CustomerControllerTest start

--- src/test/java/ga/heaven/controller/CustomerControllerTest.java
+++ src/test/java/ga/heaven/controller/CustomerControllerTest.java
@@ -1,12 +1,16 @@
 package ga.heaven.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import ga.heaven.model.Customer;
 import ga.heaven.repository.CustomerRepository;
 import ga.heaven.service.*;
+import io.swagger.v3.core.util.Json;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JsonContent;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
@@ -14,6 +18,7 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -57,6 +62,25 @@ class CustomerControllerTest {
     @MockBean
     private VolunteerService volunteerService;
 
+    private List<Customer> getInitialTestCustomers() {
+        Customer customer1 = getTestCustomer(1L, "customerName1", "+71111111111");
+        Customer customer2 = getTestCustomer(2L, "customerName2", "+72222222222");
+
+        List<Customer> customers = new ArrayList<>();
+        customers.add(customer1);
+        customers.add(customer2);
+
+        return customers;
+    }
+
+    private Customer getTestCustomer(long id, String name, String phoneNumber) {
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setName(name);
+        customer.setPhone(phoneNumber);
+        return customer;
+    }
+
     @Test
     public void getAllCustomers() throws Exception{
         List<Customer> customers = getInitialTestCustomers();
@@ -67,25 +91,16 @@ class CustomerControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
         //System.out.println("response = " + response.getContentAsString());
-    }
+        // Полезная информация https://www.baeldung.com/gson-list
 
-    private List<Customer> getInitialTestCustomers() {
-        Customer customer1 = new Customer();
-        customer1.setId(1L);
-        customer1.setName("customerName1");
-        customer1.setPhone("+71111111111");
+        Type listOfMyClassObject = new TypeToken<ArrayList<Customer>>() {}.getType();
 
-        Customer customer2 = new Customer();
-        customer2.setId(2L);
-        customer2.setName("customerName2");
-        customer2.setPhone("+72222222222");
-
-
-        List<Customer> customers = new ArrayList<>();
-        customers.add(customer1);
-        customers.add(customer2);
-
-        return customers;
+        Gson gson = new Gson();
+        List<Customer> actual = gson.fromJson(response.getContentAsString(), listOfMyClassObject);
+        /*System.out.println("actual = " + actual);
+        System.out.println("customers = " + customers);*/
+        assertThat(actual).containsExactlyInAnyOrderElementsOf(customers);
+        assertThat(actual).isEqualTo(customers);
     }
 
     @Test
