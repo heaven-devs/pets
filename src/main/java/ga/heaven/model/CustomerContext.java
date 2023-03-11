19c17b457d4f279030d7b4209ed05e3dc92dbffa
Telion <www.neon@mail.ru>
2023-03-11 17:17:04 +0500

    refactoring customer context entity

--- src/main/java/ga/heaven/model/CustomerContext.java
+++ src/main/java/ga/heaven/model/CustomerContext.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 public class CustomerContext {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private Context dialogContext;
     private long currentPetId;
